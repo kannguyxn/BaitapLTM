c9e1c35f44a8f073354982e823b9c18993358e17
Nguyễn GIa Khang <nguyengiakhang1607@gmail.com>
2026-07-11 10:01:43 +0700

    fix

--- BaitapLTM_Server/Program.cs
+++ BaitapLTM_Server/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Text;
 using System;
 
 namespace BaiTapLTM_Server
@@ -7,6 +7,8 @@ namespace BaiTapLTM_Server
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
             Server server = new Server();
 
             server.Start();
